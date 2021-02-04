444e2ee325c4b15d0c7e255870872c076fb45e31
mamezo-san <fader.con.39.0709@gmail.com>
2021-02-04 22:19:40 +0900

    change-hello-world

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+       <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hello-world
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
