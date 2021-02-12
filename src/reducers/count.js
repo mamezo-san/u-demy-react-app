702f6b50cc027975966a66bf54b6b4f531ef9916
mamezo-san <fader.con.39.0709@gmail.com>
2021-02-12 23:52:01 +0900

    implement component with connect

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -2,13 +2,13 @@ import { INCREMENT,DECREMENT } from '../actions'
 
 const initialState = { value: 0 }
 
-export defoult (state = initialState,action) => {
+export default (state = initialState,action) => {
   switch(action.type) {
     case INCREMENT:
       return { value: state.value + 1 } 
     case DECREMENT:
       return { value: state.value - 1 } 
-    defoult:
+    default:
       return state
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
 import { combineReducers } from 'redux'
 import count from './count'
 
-export defoult combineReducers({ count }) 
+export default combineReducers({ count }) 
