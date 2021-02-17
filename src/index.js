1c40b711333e9f8f8c000bc911c6079f73354d45
mamezo-san <fader.con.39.0709@gmail.com>
2021-02-17 21:56:33 +0900

    apply material-ui to eventIndex

--- src/index.js
+++ src/index.js
@@ -12,24 +12,27 @@ import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
 import EventsShow from './components/events_show';
 import reportWebVitals from './reportWebVitals';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const enhancer = process.env.NODE_ENV === 'development' ?
   composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
 const store = createStore(reducer,enhancer)
 
 ReactDOM.render(
-  <React.StrictMode>
-   <Provider store={ store }>
-    <BrowserRouter>
-     <Switch>
-      <Route path="/events/new" component={EventsNew} />
-      <Route path="/events/:id" component={EventsShow} />
-      <Route exact path="/" component={EventsIndex} />
-      <Route exact path="/events" component={EventsIndex} />
-     </Switch>
-    </BrowserRouter>
-   </Provider>
-   </React.StrictMode>,
+  <MuiThemeProvider>
+    <React.StrictMode>
+    <Provider store={ store }>
+      <BrowserRouter>
+      <Switch>
+        <Route path="/events/new" component={EventsNew} />
+        <Route path="/events/:id" component={EventsShow} />
+        <Route exact path="/" component={EventsIndex} />
+        <Route exact path="/events" component={EventsIndex} />
+      </Switch>
+      </BrowserRouter>
+    </Provider>
+    </React.StrictMode>
+  </MuiThemeProvider>, 
   document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
